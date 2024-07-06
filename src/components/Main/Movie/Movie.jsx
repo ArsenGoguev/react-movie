c7066f35881a2978ee4163bcaa61aa794ebfb4a6
ArsenGoguev <agoguev33@gmail.com>
2024-07-06 21:11:40 +0300

    3 | Перенесены стили из Movie.jsx в movie.css, загружены шрифты

--- src/components/Main/Movie/Movie.jsx
+++ src/components/Main/Movie/Movie.jsx
@@ -18,38 +18,18 @@ export default function Movie({ overview, date, poster, title }) {
     description = overview.slice(0, 190) + '...'
   }
 
-  const imgStyle = {
-    display: 'block',
-    width: 183,
-    height: 280,
-    borderRadius: 0,
-    minHeight: '100%',
-  }
-
-  const cardStyle = {
-    paddingTop: 0,
-    boxShadow: '0px 4px 12px 0px #00000026',
-    border: 0,
-    borderRadius: 0,
-    display: 'flex',
-    width: 450,
-    height: 280,
-  }
-
   return (
-    <Card style={cardStyle} size="default" cover={<img alt="Movie cover" src={posterLink} style={imgStyle} />}>
+    <Card className="movie" size="default" cover={<img className="movie__cover" alt="Movie cover" src={posterLink} />}>
       <Flex vertical gap={7}>
-        <Typography.Title level={3} style={{ margin: 0 }}>
+        <Typography.Title className="movie__title" level={3}>
           {title}
         </Typography.Title>
-        <span style={{ fontSize: 12, color: '#827E7E' }}>
-          {date ? format(date, 'PP') : `Release date is not found`}
-        </span>
+        <span className="movie__release-date">{date ? format(date, 'PP') : `Release date is not found`}</span>
         <Flex>
           <Tag>Genre</Tag>
         </Flex>
 
-        <Typography style={{ minWidth: 'auto', fontSize: 12 }}>
+        <Typography className="movie__overview">
           {description ? description : 'This movie has no description'}
         </Typography>
       </Flex>

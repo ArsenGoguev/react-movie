b8c5ad7890b7642a24f2cf8cf92d5eae4433c2d4
ArsenGoguev <agoguev33@gmail.com>
2024-07-14 01:42:51 +0300

    10 | Добавлены экран загрузки данных и предупреждение о некорректном введенном названии

--- src/components/Movies/MovieList.jsx
+++ src/components/Movies/MovieList.jsx
@@ -1,19 +1,19 @@
 import React, { useContext } from 'react'
 import { Flex } from 'antd'
-import { Alert } from 'antd'
+import { Alert, Spin } from 'antd'
 
 import { MoviesAppContext } from '../Context/Context.js'
 
 import Movie from './Movie/Movie.jsx'
 
 export default function MovieList() {
-  const { movies, loading, error } = useContext(MoviesAppContext)
+  const { movies, loading, error, searchingMovie } = useContext(MoviesAppContext)
 
   const movieList = movies.map((mov) => {
     return <Movie key={mov.id} movie={mov} loading={loading} />
   })
 
-  if (error)
+  if (error) {
     return (
       <Alert
         style={{ marginTop: 34, marginBottom: 34 }}
@@ -22,10 +22,24 @@ export default function MovieList() {
         description="Error! Something is wrong."
       />
     )
-
-  return (
-    <Flex style={{ marginTop: 34, marginBottom: 34 }} gap={36} wrap justify="space-between">
-      {movieList}
-    </Flex>
-  )
+  } else if (movies.length === 0 && !error) {
+    if (searchingMovie === 'popular') {
+      return <Spin fullscreen style={{ marginTop: 34, marginBottom: 34 }} size="large" tip="Loading data" />
+    } else {
+      return (
+        <Alert
+          style={{ marginTop: 34, marginBottom: 34 }}
+          type="warning"
+          message={'No movies were found for your search'}
+          description="You may have entered the wrong movie title. Please try to enter a different title."
+        />
+      )
+    }
+  } else {
+    return (
+      <Flex style={{ marginTop: 34, marginBottom: 34 }} gap={36} wrap justify="space-between">
+        {movieList}
+      </Flex>
+    )
+  }
 }
